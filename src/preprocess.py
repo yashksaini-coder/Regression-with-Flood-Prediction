300d5e89b070d6a5a2adc24c6e4754f7b7950c9d
yashksaini-coder <ys3853428@gmail.com>
2024-05-03 23:07:34 +0530

    Modify printINFO function

--- src/preprocess.py
+++ src/preprocess.py
@@ -8,15 +8,15 @@ from colorama import Style, Fore
 def printInfo(df,train,test):
     print(f'{Style.BRIGHT}{Fore.YELLOW}SHAPE{Style.RESET_ALL}')
     print(f'{Style.BRIGHT}{Fore.GREEN} train: {train.shape}')
-    print(f'{Style.BRIGHT}{Fore.GREEN} test:  {test.shape}')
+    print(f'{Style.BRIGHT}{Fore.BLUE} test:  {test.shape}')
     print(f'{Style.BRIGHT}{Fore.GREEN} original:  {df.shape}')
     print(f'{Style.BRIGHT}{Fore.YELLOW}\nNULL VALUES{Style.RESET_ALL}')
     print(f'{Style.BRIGHT}{Fore.GREEN} train: {train.isnull().any().any()}')
-    print(f'{Style.BRIGHT}{Fore.GREEN} train: {test.isnull().any().any()}')
+    print(f'{Style.BRIGHT}{Fore.BLUE} test: {test.isnull().any().any()}')
     print(f'{Style.BRIGHT}{Fore.GREEN} original: {df.isnull().any().any()}')    
     print(f'{Style.BRIGHT}{Fore.YELLOW}\nDUPLICATES{Style.RESET_ALL}')
     print(f'{Style.BRIGHT}{Fore.GREEN} train: {train.duplicated().any().any()}')
-    print(f'{Style.BRIGHT}{Fore.GREEN} train: {test.duplicated().any().any()}')
+    print(f'{Style.BRIGHT}{Fore.BLUE} test: {test.duplicated().any().any()}')
     print(f'{Style.BRIGHT}{Fore.GREEN} original: {df.duplicated().any().any()}')
     
     
