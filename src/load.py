5770cd70d488bb5deabdc163bb094b9e0cd1f89a
yashksaini-coder <ys3853428@gmail.com>
2024-05-08 13:15:20 +0530

    Update load.py

--- src/load.py
+++ src/load.py
@@ -1,17 +1,17 @@
 import pandas as pd
 
 def load_train():
-    train = pd.read_csv('./data/train.csv')
+    train = pd.read_csv('../data/train.csv')
     return train
 
 def load_test():
-    test = pd.read_csv('./data/test.csv')
+    test = pd.read_csv('../data/test.csv')
     return test
 
 def load_original():
-    original = pd.read_csv('./data/origianl.csv')
+    original = pd.read_csv('../data/origianl.csv')
     return original
 
 def load_submission():
-    submission = pd.read_csv('./data/sample_submission.csv')
+    submission = pd.read_csv('../data/sample_submission.csv')
     return submission
