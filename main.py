1e4286c0c8eca625e72600ac72a1b51c0ce8a87a
yashksaini-coder <ys3853428@gmail.com>
2024-05-22 20:20:12 +0530

    Changed libraries

--- main.py
+++ main.py
@@ -1,44 +1,14 @@
-import numpy as np
+# Importing the basic libraries
 import pandas as pd
-import matplotlib.pyplot as plt
 import seaborn as sns
-import optuna
+
+from pandas.plotting import register_matplotlib_converters
 import warnings
-import scipy
-from sklearn.compose import TransformedTargetRegressor
-from sklearn import set_config
-from colorama import Style, Fore
-from sklearn.inspection import permutation_importance, PartialDependenceDisplay
-from sklearn.model_selection import StratifiedKFold, KFold
-from xgboost import XGBRegressor
-from sklearn.linear_model import Ridge, LinearRegression
-from lightgbm import LGBMRegressor
-from category_encoders import TargetEncoder, OneHotEncoder, MEstimateEncoder, OrdinalEncoder
-from sklearn.feature_selection import SequentialFeatureSelector
-from sklearn.metrics import roc_auc_score, roc_curve, make_scorer, mean_squared_log_error, r2_score
-from sklearn.metrics.pairwise import euclidean_distances
-from sklearn.pipeline import Pipeline, make_pipeline
-from sklearn.base import BaseEstimator, TransformerMixin, RegressorMixin, clone
-from sklearn.preprocessing import FunctionTransformer, StandardScaler, LabelEncoder, LabelBinarizer, MinMaxScaler, PolynomialFeatures, SplineTransformer
-from sklearn.compose import ColumnTransformer
-from scipy.cluster.hierarchy import dendrogram, linkage
-from scipy.spatial.distance import squareform
-from catboost import CatBoostRegressor
 
-palette = ["d9ed92","b5e48c","99d98c","76c893","52b69a","34a0a4","168aad","1a759f","1e6091","184e77"]
-
-config = {
-    'SEED' : 42,
-    'N_SPLITS': 5,
-    'SUBMIT' : True,
-    'USE_ORIGINAL': False
-    
-}
-
-sns.set_theme(style = 'white', palette = 'colorblind')
 pal = sns.color_palette('colorblind')
 
 pd.set_option('display.max_rows', 100)
-set_config(transform_output = 'pandas')
+pd.set_option('plotting.backend', 'matplotlib')
+register_matplotlib_converters()
 pd.options.mode.chained_assignment = None
 warnings.simplefilter(action='ignore', category=FutureWarning)
